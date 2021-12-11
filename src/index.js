1eebf907907d9c8dded07c971d41959ed1da7d70
Javier Cisneros <fjca185@gmail.com>
2021-12-10 23:07:48 -0600

    Cambio a puerto 3000

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express'); //Se solicta el modulo express
 const app = express(); // se crea el servidor 
 const path = require('path'); //Se solicita el modulo path para trabajar en directorios
 //Setting de servidor 
-app.set('port', 4000); //Variable del puerto servidor algunos host pueden tener un puerto determinado
+app.set('port', 3000); //Variable del puerto servidor algunos host pueden tener un puerto determinado
 app.set('view engine', 'ejs');//Se configura el uso de ejs para traducir js a html
 app.set('views',path.join(__dirname, 'views'));//Se agrega el path de view ya que esta debajo de src
 app.engine('html', require('ejs').renderFile); //Renderizar los archivos html con ejs
@@ -21,5 +21,5 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 //Puerto en el que escucha el servidor
 app.listen(app.get('port'), () =>{
-    console.log('Listening on 4000', app.get('port'));
+    console.log('Listening on 3000', app.get('port'));
 });
